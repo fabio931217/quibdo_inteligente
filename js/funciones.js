11ac0ad96b8ec45a87a9d2effcc415e9e0a99262
fabio931217 <ingeniero.fabio.garcia@gmail.com>
2017-03-21 22:09:46 -0500

    version9
    version9

--- js/funciones.js
+++ js/funciones.js
@@ -1,6 +1,5 @@
 function obtenert_publicaciones() {
 	Get_Ajax('Publicaciones/listado_publicaciones',{},false,function(data){
-		alert(data.dataObj);
 		if(data.res){
 			$("#recibe_publicaciones").html(data.dataObj);
 			QuitarLoading();
